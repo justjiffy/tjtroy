fe770871c80ec17d4ebe60cacfc3f606fd4a109c
Juli Emmel <justjiffy@gmail.com>
2015-11-27 15:48:42 -0800

    javascript and jquery running

---
+++ vo/README.md
@@ -0,0 +1,1 @@
+# TJ-Troy-Voiceover
---
+++ vo/script.js
@@ -0,0 +1,58 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+// fade transition animation 
+ $(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    inDuration: 1500,
+    outDuration: 800,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
+    loading: true,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
+ // end fade transition
+});
+
+
+
+
+// SCROLL ANIMATION 
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+// end scroll animation
+
+    $('div.panel').hover(function(){
+    		$( this ).addClass('clicked');
+    		$('.about_text').toggle(500);
+    		$('.about_image').toggle(500);
+    	});
+
+
+
+
